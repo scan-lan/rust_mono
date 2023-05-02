09d1c53bc40a25dcbe9a9ba3c5f354bc20b0ceb3
Luke Scanlan <luke@lukescanlan.com>
2023-05-02 11:12:01 +0100

    Add day 1 part 2

--- aoc/src/y2015/day01.rs
+++ aoc/src/y2015/day01.rs
@@ -14,11 +14,35 @@ pub fn part_1() -> i32 {
     })
 }
 
+pub fn part_2() -> i32 {
+    INPUT.chars().enumerate().fold(0, |mut floor, (i, el)| {
+        if floor == -1 {
+            println!("{}", i)
+        }
+        match el {
+            '(' => {
+                floor += 1;
+                floor
+            }
+            ')' => {
+                floor -= 1;
+                floor
+            }
+            _ => floor,
+        }
+    })
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn test_part_1() {
         println!("Day 1; pt 1: {}", part_1());
+    }
+
+    #[test]
+    fn test_part_2() {
+        println!("Day 1; pt 2: {}", part_2());
     }
 }
