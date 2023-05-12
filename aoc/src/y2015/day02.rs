d5c3554156f9fc82e7bb984411f7563167625eac
Luke Scanlan <luke@lukescanlan.com>
2023-05-12 16:44:08 +0100

    Add day 2 part 2

--- aoc/src/y2015/day02.rs
+++ aoc/src/y2015/day02.rs
@@ -1,84 +1,110 @@
 use itertools::Itertools;
 
-static INPUT: &'static str = include_str!("./day02.txt");
+use std::sync::Once;
 
-struct GiftBox {
-    l: u32,
-    w: u32,
-    h: u32,
+static INPUT: &'static str = include_str!("./day02.txt");
+static INIT: Once = Once::new();
+static mut BOXES: Vec<GiftBox> = Vec::new();
+
+/// Get a static reference to list of gift boxes
+fn input_gift_boxes() -> &'static Vec<GiftBox> {
+    unsafe {
+        INIT.call_once(|| {
+            BOXES.extend(
+                INPUT
+                    .lines()
+                    .map(|line| {
+                        let dimensions = line
+                            .split('x')
+                            .map(|d| d.parse::<u64>().expect("Invalid data entered"))
+                            .collect_vec();
+
+                        GiftBox::from(dimensions)
+                    })
+                    .collect_vec(),
+            )
+        });
+        &BOXES
+    }
 }
 
-impl From<Vec<u32>> for GiftBox {
-    fn from(value: Vec<u32>) -> Self {
+impl From<Vec<u64>> for GiftBox {
+    fn from(value: Vec<u64>) -> Self {
         let mut value = value.iter();
 
+        let (l, w, h) = (
+            *value.next().expect("Invalid data entered"),
+            *value.next().expect("Invalid data entered"),
+            *value.next().expect("Invalid data entered"),
+        );
+
+        let combos = [l, w, h].into_iter().combinations(2);
+
+        let surface_area = combos
+            .clone()
+            .map(|c| c.into_iter().product::<u64>() * 2)
+            .sum();
+
+        let slack = combos
+            .clone()
+            .map(|c| c.into_iter().product())
+            .min()
+            .expect("Should be a min");
+
+        let bow = [l, w, h].into_iter().product::<u64>();
+
+        let ribbon_required = combos
+            .clone()
+            // Map to perimeters
+            .map(|c| c.into_iter().sum::<u64>() * 2)
+            .min()
+            .expect("Min perimeter")
+            + bow;
+
         GiftBox {
-            l: *value.next().expect("Invalid data entered"),
-            w: *value.next().expect("Invalid data entered"),
-            h: *value.next().expect("Invalid data entered"),
+            l,
+            w,
+            h,
+            surface_area,
+            slack,
+            ribbon_required,
         }
     }
 }
 
-impl GiftBox {
-    fn surface_area(&self) -> u32 {
-        2 * self.l * self.w + 2 * self.w * self.h + 2 * self.h * self.l
-    }
-
-    fn min_area(&self) -> u32 {
-        [self.l, self.w, self.h]
-            .iter()
-            .combinations(2)
-            .map(|c| c.into_iter().product())
-            .min()
-            .expect("Should be a min")
-    }
+struct GiftBox {
+    l: u64,
+    w: u64,
+    h: u64,
+    surface_area: u64,
+    slack: u64,
+    ribbon_required: u64,
 }
 
-pub fn part_1() -> u32 {
-    INPUT.lines().fold(0, |mut tot, el| {
-        let dimensions: Vec<u32> = el
-            .split('x')
-            .map(|d| d.parse::<u32>().unwrap_or(1))
-            .collect();
+// impl GiftBox {
+//     fn surface_area(&self) -> u64 {
+//         self.surface_area
+//     }
+//
+//     fn slack(&self) -> u64 {
+//         self.slack
+//     }
+//
+//     fn ribbon_required(&self) -> u64 {
+//         self.slack
+//     }
+// }
 
-        let areas = dimensions
-            .into_iter()
-            .combinations(2)
-            .map(|side| side.into_iter().product());
+pub fn part_1() -> u64 {
+    let boxes = input_gift_boxes();
 
-        let slack = areas.clone().min().expect("There will be a min");
-        let box_area = areas.fold(0, |mut acc, area: u32| {
-            acc += 2 * area;
-            acc
-        });
-
-        tot += box_area + slack;
-        tot
-    })
+    boxes.iter().map(|bx| bx.surface_area + bx.slack).sum()
 }
 
-pub fn part_2() -> i32 {
-    INPUT.lines().fold(0, |mut tot, el| {
-        let dimensions: Vec<u32> = el
-            .split('x')
-            .map(|d| d.parse::<u32>().unwrap_or(1))
-            .collect();
+pub fn part_2() -> u64 {
+    let boxes = input_gift_boxes();
 
-        let areas = dimensions
-            .into_iter()
-            .combinations(2)
-            .map(|side| side.into_iter().product());
-
-        let slack = areas.clone().min().expect("There will be a min");
-        let box_area = areas.fold(0, |mut acc, area: u32| {
-            acc += 2 * area;
-            acc
-        });
-
-        tot += box_area + slack;
-        tot
-    })
+    boxes.iter().map(|bx| bx.ribbon_required).sum()
 }
 
 #[cfg(test)]
@@ -89,8 +115,8 @@ mod test {
         println!("Day 2; pt 1: {}", part_1());
     }
 
-    // #[test]
-    // fn test_part_2() {
-    //     println!("Day 2; pt 2: {}", part_2());
-    // }
+    #[test]
+    fn test_part_2() {
+        println!("Day 2; pt 2: {}", part_2());
+    }
 }
